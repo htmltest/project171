ed2a38d48c3eb57f31100637c488a3c38e894cc7
htmltest <struev.vs@gmail.com>
2020-11-14 17:15:55 +0300

    first html

---
+++ js/fonts.js
@@ -0,0 +1,67 @@
+var html = document.documentElement;
+
+var fontsfile = document.createElement('link');
+fontsfile.href = pathTemplate + 'css/fonts.css';
+fontsfile.rel = 'stylesheet';
+document.head.appendChild(fontsfile);
+
+if (sessionStorage.fontsLoaded) {
+    html.classList.add('fonts-loaded');
+} else {
+    var script = document.createElement('script');
+    script.src = pathTemplate + 'js/fontfaceobserver.js';
+    script.async = true;
+
+    script.onload = function () {
+        var Inter300 = new FontFaceObserver('Inter', {
+            weight: '300'
+        });
+        var Inter400 = new FontFaceObserver('Inter', {
+            weight: 'normal'
+        });
+        var Inter500 = new FontFaceObserver('Inter', {
+            weight: '500'
+        });
+        var Inter600 = new FontFaceObserver('Inter', {
+            weight: '600'
+        });
+        var FiraSans300 = new FontFaceObserver('FiraSans', {
+            weight: '300'
+        });
+        var FiraSans400 = new FontFaceObserver('FiraSans', {
+            weight: 'normal'
+        });
+        var FiraSans600 = new FontFaceObserver('FiraSans', {
+            weight: '600'
+        });
+        var FiraSans700 = new FontFaceObserver('FiraSans', {
+            weight: 'bold'
+        });
+        var FiraSans800 = new FontFaceObserver('FiraSans', {
+            weight: '800'
+        });
+        var JetBrainsMono500 = new FontFaceObserver('JetBrainsMono', {
+            weight: '500'
+        });
+        var JetBrainsMono700 = new FontFaceObserver('JetBrainsMono', {
+            weight: 'bold'
+        });
+
+        Promise.all([
+            Inter300.load(),
+            Inter400.load(),
+            Inter500.load(),
+            FiraSans300.load(),
+            FiraSans400.load(),
+            FiraSans600.load(),
+            FiraSans700.load(),
+            FiraSans800.load(),
+            JetBrainsMono500.load(),
+            JetBrainsMono700.load()
+        ]).then(function () {
+            html.classList.add('fonts-loaded');
+            sessionStorage.fontsLoaded = true;
+        });
+    };
+    document.head.appendChild(script);
+}
---
+++ js/tools.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    $('.gallery').each(function() {
+        var curGallery = $(this);
+        curGallery.on('init', function(event, slick) {
+            var curSlide = curGallery.find('.slick-current');
+            var curPhotoHeight = curSlide.find('.gallery-item-photo').outerHeight();
+            curGallery.find('.slick-prev').css({'top': curPhotoHeight / 2});
+            curGallery.find('.slick-next').css({'top': curPhotoHeight / 2});
+        });
+        curGallery.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: '<button type="button" class="slick-prev"><svg><use xlink:href="' + pathTemplate + 'images/sprite.svg#gallery-prev"></use></svg></button>',
+            nextArrow: '<button type="button" class="slick-next"><svg><use xlink:href="' + pathTemplate + 'images/sprite.svg#gallery-next"></use></svg></button>',
+            adaptiveHeight: true,
+            fade: true,
+            dots: false
+        }).on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            var curSlide = curGallery.find('.slick-slide:not(.slick-cloned)').eq(nextSlide);
+            var curPhotoHeight = curSlide.find('.gallery-item-photo').outerHeight();
+            curGallery.find('.slick-prev').css({'top': curPhotoHeight / 2});
+            curGallery.find('.slick-next').css({'top': curPhotoHeight / 2});
+        });
+    });
+
+    $('.slider-list').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        arrows: false,
+        dots: true
+    });
+
+    $('.main-how-menu-item a').click(function(e) {
+        var curItem = $(this).parent();
+        if (!curItem.hasClass('active')) {
+            $('.main-how-menu-item.active').removeClass('active');
+            curItem.addClass('active');
+
+            var curIndex = $('.main-how-menu-item').index(curItem);
+            $('.main-how-tab.active').removeClass('active');
+            $('.main-how-tab').eq(curIndex).addClass('active');
+        }
+        e.preventDefault();
+    });
+
+});
